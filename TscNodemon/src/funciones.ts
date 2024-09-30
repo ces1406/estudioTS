26bf1343a731850d621e747d0e5c67e17d50b5e8
cesar-win10-pc <ces1406@gmail.com>
2024-09-30 11:17:27 -0300

    funciones genericas

--- TscNodemon/src/funciones.ts
+++ TscNodemon/src/funciones.ts
@@ -1,3 +1,19 @@
 function promedio(n1:number,n2:number):number{
     return (n1+n2)/2
 }
+/**
+ * Funciones generica
+ */
+function reversa <T> (lista:T[]):T[] { // se indica el tipo generico con: <T>
+    let listaRta : T[] = [];
+    for(let i=lista.length;i>0;i--){
+        listaRta.push(lista[i-1])
+    }
+    return listaRta;
+}
+
+const vec0: string[]=['uno','dos','tres'];
+const vec1: number[]=[1,2,3];
+
+console.log('reversa(vec0): ',reversa(vec0));
+console.log('reversa(vec1): ',reversa(vec1));
